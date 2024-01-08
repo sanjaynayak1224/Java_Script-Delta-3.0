fc5e887699fbc1b7ba4e9195d1b355baa03f253d
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-08 11:16:49 +0530

    Variables Done

---
+++ 01-Variables.js
@@ -0,0 +1,18 @@
+a=25
+console.log(typeof a)
+Name='sanjay'
+console.log(typeof Name)
+b=5.88
+console.log(typeof b)
+c=0.99999999999999999999999999999999999999999999
+//prints 1 as output
+console.log(c)
+console.log(2**8)
+console.log(12%5)
+console.log(12/5)
+console.log(0/0) //output prints NaN
+a=NaN
+console.log(a-1)
+console.log(a*2)
+
+
