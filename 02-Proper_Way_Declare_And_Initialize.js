87cd5125b4134a63d9bb46fdf8886b3a28971722
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-09 06:34:23 +0530

    Done

---
+++ 02-Proper_Way_Declare_And_Initialize.js
@@ -0,0 +1,12 @@
+let a=5;
+console.log(a);
+let b;
+b=7;
+console.log(b);
+let num1 = 1;
+let num2 =2;
+let sum=num1+num2
+console.log(sum)
+const year=2025; //the value of this varibale cannot be changed
+//year=2; doesnt work
+var age=18;
