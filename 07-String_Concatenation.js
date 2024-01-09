c2b221bb5afbab39ddef2d3572ad3962fd98ee8b
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-09 07:20:11 +0530

    String Concatenation Done

---
+++ 07-String_Concatenation.js
@@ -0,0 +1,7 @@
+console.log("sanjay" + " " + "nayak")
+console.log("sanjay" + "1")
+console.log("sanjay" + 5)
+let firstName="Sanjay"
+let lastName=" Nayak"
+let fullName=firstName+lastName
+console.log(fullName)
