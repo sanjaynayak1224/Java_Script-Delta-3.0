baca583db6afb50d53b544d6652da012af7b2b97
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-09 06:57:49 +0530

    Strings Done

---
+++ 05-Strings.js
@@ -0,0 +1,11 @@
+let firstName="Sanjay";
+console.log(typeof firstName)
+firstName='Sanjay';
+console.log(typeof firstName)
+let Name='a'
+console.log(typeof Name)
+Name=''
+console.log(typeof Name)
+let num='123'
+console.log(num)
+console.log(typeof num)
