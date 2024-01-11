a9cc881da78180d054a839791f0145a80427f3ef
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-11 09:25:41 +0530

    Done

---
+++ 02-Java_Script_Html/app.js
@@ -0,0 +1,18 @@
+// console.log("Hello world");
+let a=5;
+let b=9;
+console.log("The Sum is :",a+b,"rupees");
+console.log(`The Sum is : ${a+b} rupees`);
+
+
+//operators
+let age=18;
+console.log(age>18)  //false
+console.log(age==18) //true
+console.log(age!=18) //false
+console.log(5=='5')  //true
+console.log(3==='3')  //compares both datatype and values i.e false
+
+
+//comparison of non-numbers
+console.log('a'<'A')  //false because unicode of 'a'(i.e 61) is greater than the unicode of 'A'(i.e 41)
