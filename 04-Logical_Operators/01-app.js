36384a49778ceb45a121d40923f01bb3d75efbed
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-11 10:27:47 +0530

    Logical Operators Done

---
+++ 04-Logical_Operators/01-app.js
@@ -0,0 +1,26 @@
+let marks=100;
+let age=18;
+if(marks==100 && age==18){
+    console.log("Both Condtions are True");
+}
+else{
+    console.log("check the condition again");
+}
+
+if(marks==50 || age==18){
+    console.log("Either Both or one of the Condtions is True");
+}
+else{
+    console.log("Both Condtions are false");
+}
+
+
+
+if(!(marks==50 || age==18)){
+    console.log("Either Both or one of the Condtions is True");
+}
+else{
+    console.log("Both Condtions are false");
+}
+
+
