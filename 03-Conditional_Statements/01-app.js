73a0c05e5a9b15797907c3046379c61ec65b0cdb
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-11 09:49:52 +0530

    Done

---
+++ 03-Conditional_Statements/01-app.js
@@ -0,0 +1,19 @@
+let age=23;
+if(age>=18){
+    console.log("Eligible to vote");
+}
+
+else{
+    console.log("Not eligible to vote");
+}
+
+let firstName="Sanjay";
+if(firstName=="Sanjay"){
+    console.log(`Welcome ${firstName}`);
+}
+
+else{
+    console.log("You are not welcome here");
+}
+
+//else if statement also exists in javascript like other programing languages
