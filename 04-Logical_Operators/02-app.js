1add28d999be27b384577766e0a13be71d391c63
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-12 19:45:20 +0530

    Practice Question Done

---
+++ 04-Logical_Operators/02-app.js
@@ -0,0 +1,8 @@
+let good="abcde";
+if((good[0]==="a") && (good.length>3)){
+    console.log("The Given String is a good string");
+}
+
+else{
+    console.log("The given string is not a good string");
+}
