14c91a82d3099e9ae7a640c4933dcf4274dd0556
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-12 19:54:31 +0530

    Switch Statement Done

---
+++ 05-Switch_Statement/01-app.js
@@ -0,0 +1,15 @@
+let color="green";
+switch(color){
+    case "red":
+        console.log("stop");
+        break;
+    case "yellow":
+        console.log("slow down");
+         break;
+    case "green":
+        console.log("GO");
+        break;
+    default:
+        console.log("Broken Light")    
+        
+}
