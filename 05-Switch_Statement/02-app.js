6edc319e60138be83b551e0775409e4457dd8c8b
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-25 09:48:11 +0530

    Switch Statement Done

---
+++ 05-Switch_Statement/02-app.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+// alert("Something went wrong");
+console.error("hello");
+console.warn("This is a warning message");
+let firstName=prompt("Enter your first name");
+console.log(firstName);
+let lastName=prompt("Enter your last name");
+console.log(lastName);
+
+
+
+// practice question
+/*
+let num1=32;
+let num2=47852;
+if(num1%10==2 && num2%10==2){
+    console.log("both the number have the last digit as 2");
+}
+
+else{
+    console.log("Only one of them or both the numbers doesnt have last digit as 2")
+}
+*/
