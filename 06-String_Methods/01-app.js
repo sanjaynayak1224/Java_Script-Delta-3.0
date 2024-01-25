04df7e11f90e93b734b599f05d8b13d1506d276a
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-25 10:33:08 +0530

    String Methods Done

---
+++ 06-String_Methods/01-app.js
@@ -0,0 +1,18 @@
+let msg="    hello    ";
+console.log(msg);
+console.log(msg.trim());
+let Name="Sanjay"
+console.log(Name.toUpperCase());
+console.log(Name.toLowerCase());
+let fullName="Sanjay S Nayak";
+console.log(fullName.indexOf('y'));
+let multiple="     saNjay s nAyAK      ";
+console.log(multiple);
+console.log(multiple.toUpperCase().trim());
+console.log(multiple.toLowerCase().trim()); 
+let str="ILoveCoding";
+console.log(str.slice(1,5));
+let str1="ILoveCoding";
+console.log(str1.replace("Love","Do"));
+let str2="mango ";
+console.log(str2.repeat(10));
