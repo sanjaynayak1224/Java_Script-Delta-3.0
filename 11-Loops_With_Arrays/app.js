68de085784d81eddbd326af335b3026e902c4826
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-28 10:09:15 +0530

    Done

---
+++ 11-Loops_With_Arrays/app.js
@@ -0,0 +1,13 @@
+let fruits=["Mango","Apple","Banana","Litchi","Orange"]
+for(let i=0;i<fruits.length;i++){
+    console.log(`${i}:${fruits[i]}`);
+}
+let equal="=";
+console.log(equal.repeat(50));
+let hero=[["Spiderman","Batman","Antman"],["Captain","Thor","wanda"]]
+for(let i=0;i<hero.length;i++){
+    console.log(i);
+    for(let j=0;j<hero[i].length;j++){
+        console.log(hero[i][j]);
+    }
+}
