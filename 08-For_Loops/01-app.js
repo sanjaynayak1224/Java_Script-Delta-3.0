7fad635596ea8aeabf6e67d67694267d4f66ffa4
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-28 09:34:27 +0530

    Done

---
+++ 08-For_Loops/01-app.js
@@ -0,0 +1,45 @@
+/*
+for(let i=1;i<=500;i++){
+    console.log(i);
+}
+*/
+
+//print odd numbers
+console.log("Odd:");
+for(let i=0;i<10;i++){
+    if(i%2!=0){
+        console.log(i);
+    }
+}
+
+
+//print even numbers
+console.log("Even:");
+let i=0;
+for(let i=0;i<10;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+//multiplication table of of given number
+// let n=prompt("Enter the Number whose tables has to be printed:");
+// n=parseInt(n);
+// console.log(`Multiplication Table Of ${n}:`)
+// for(let i=1;i<=10;i++){
+//     console.log(n*i);
+// }
+
+
+//nested for loop
+for(let i=1;i<=5;i++){
+    console.log(i);
+    for(let j=1;j<=4;j++){
+        console.log("Hello World");
+    }
+}
+
+
+
+
+
