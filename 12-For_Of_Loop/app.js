e2031f65beebdf35d8f943af4365555f5f540b42
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-28 10:16:41 +0530

    Done

---
+++ 12-For_Of_Loop/app.js
@@ -0,0 +1,10 @@
+let fruits=["mango","apple","banana","litchi","orange"];
+for(i of fruits){
+    console.log(i);
+}
+
+for(j of "apnacollege"){
+    console.log(j);
+}
+
+//similarly nested for of loop can also be achieved
