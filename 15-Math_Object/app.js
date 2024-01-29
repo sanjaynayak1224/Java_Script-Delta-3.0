669bbf427c504b433b404720a0f37e1262022e9f
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-29 12:26:28 +0530

    Done

---
+++ 15-Math_Object/app.js
@@ -0,0 +1,14 @@
+for(let i=1;i<=10;i++){
+    let num=Math.random()*10;
+    num=Math.ceil(num);
+    console.log(num);
+}
+
+//or shprtcut
+console.log("=".repeat(73));
+for(let i=1;i<=10;i++){
+    console.log(Math.ceil(Math.random()*10));
+}
+
+
+
