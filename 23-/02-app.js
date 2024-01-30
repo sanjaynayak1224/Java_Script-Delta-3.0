2d52e78bf0eb2b50acf2ca319854db506f7ae004
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2024-01-30 17:53:17 +0530

    Done

---
+++ 23-/02-app.js
@@ -0,0 +1,14 @@
+console.log("hello");
+console.log("hello");
+console.log("hello");
+// let a=5;
+try{
+    console.log(a);
+}
+catch(err){
+    console.log("This variable doesnt exist");
+    console.log(err);
+}
+console.log("hello2");
+console.log("hello2");
+console.log("hello2");
